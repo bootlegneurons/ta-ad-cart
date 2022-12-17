057d44e08356308da7009acdd66fdf1c77271fe1
David Dembo <bootlegneurons@gmail.com>
2022-12-17 14:45:29 +1100

    stub mock data, mock fetcher and types

---
+++ src/features/cart/mock__data.ts
@@ -0,0 +1,5 @@
+import { Products, Carts, Offers } from './types';
+
+export const products: Products = {};
+export const carts: Carts = {};
+export const offers: Offers = {};
---
+++ src/features/cart/types.ts
@@ -0,0 +1,8 @@
+type Product = {};
+export type Products = Record<string, Product>;
+
+type Cart = {};
+export type Carts = Record<string, Cart>;
+
+type Offer = {};
+export type Offers = Record<string, Offer>;
