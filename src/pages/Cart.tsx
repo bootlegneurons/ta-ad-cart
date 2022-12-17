9d8084704c8c326cfb26f8026089fbdaae796e7f
David Dembo <bootlegneurons@gmail.com>
2022-12-17 18:56:35 +1100

    simplify cart page

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Checkbox, Flex, Heading, Link, Text } from '@chakra-ui/react';
+import { Box, Button, Checkbox, Divider, Flex, Heading, Link, Text } from '@chakra-ui/react';
 import React from 'react';
 import useCart from '../features/cart/useCart';
 
@@ -10,33 +10,38 @@ const Cart = (): JSX.Element => {
     <Flex w='100vw' h='100vh' justify='center' align='center'>
       <Flex direction='column' maxW={768} border='1px' gap='16px' padding='16px'>
         <Heading as='h1'>Checkout for {customer}</Heading>
-        <Text>Cart Id: {cartId}</Text>
-        <Heading size='md'>Your cart</Heading>
+        <Heading size='md'>Your cart (id: {cartId})</Heading>
         {items.map(item => {
           const product = Object.values(item)[0];
           return (
             <div key={product.name}>
               <Heading as='h3' size='sm'>
-                {product.name}
+                {product.quantity}x {product.name}
               </Heading>
               <Text>{product.description}</Text>
-              <Text>Qty: {product.quantity}</Text>
               <Text>Price: ${product.price}</Text>
             </div>
           );
         })}
-        <Heading size='md'>Summary</Heading>
+        <Divider />
         <Text>Subtotal: {subtotal}</Text>
+        {offers.length > 0 && (
+          <>
+            <Heading as='h3' size='sm'>
+              Offers applied
+            </Heading>
+            {offers.map(offer => (
+              <Text key={offer.description}>
+                {offer.description} (-${offer.discountAmount})
+              </Text>
+            ))}
+          </>
+        )}
+        <Text>Discount: -${discount}</Text>
         <Heading as='h3' size='sm'>
-          Offers applied
+          Total: ${total}
         </Heading>
-        {offers.map(offer => (
-          <Text key={offer.description}>
-            Discount applied: {offer.description} (-${offer.discountAmount})
-          </Text>
-        ))}
-        <Text>Discount total: -${discount}</Text>
-        <Heading as='h3' size='sm'>Total: ${total}</Heading>
+        <Divider />
         <Box>
           <Checkbox>I agree to the SEEK Advertising Terms & Conditions</Checkbox> (
           <Link>click to read</Link>)
