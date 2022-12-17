a2e9d30e17461139d89a6821113b7cbfe0cc0d04
David Dembo <bootlegneurons@gmail.com>
2022-12-17 14:34:05 +1100

    add skeleton for Cart page and wrap in ChakraProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Cart from './pages/Cart';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Cart />
+    <ChakraProvider>
+      <Cart />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,15 +1,20 @@
-import { Container, Flex, Heading } from '@chakra-ui/react';
+import { Box, Button, Checkbox, Flex, Heading, Link } from '@chakra-ui/react';
 import React from 'react';
 
 function App() {
   return (
     <Flex w='100vw' h='100vh' justify='center' align='center'>
-      <Container maxW={768}>
-        <Heading as="h1">Checkout</Heading>
-        <Heading>Customer details</Heading>
-        <Heading>Your cart</Heading>
-        <Heading>Summary</Heading>
-      </Container>
+      <Flex direction="column" maxW={768} border="1px" gap="16px" padding="16px">
+        <Heading as='h1'>Checkout</Heading>
+        <Heading size="md">Customer details</Heading>
+        <Heading size="md">Your cart</Heading>
+        <Heading size="md">Summary</Heading>
+        <Box>
+          <Checkbox>I agree to the SEEK Advertising Terms & Conditions</Checkbox>{' '}
+          (<Link>click to read</Link>)
+        </Box>
+        <Button disabled>Proceed to payment</Button>
+      </Flex>
     </Flex>
   );
 }
