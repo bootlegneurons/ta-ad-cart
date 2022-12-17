c8a3cd90915cee487da1ae154ca8cc32f2169101
David Dembo <bootlegneurons@gmail.com>
2022-12-17 15:14:52 +1100

    flesh out types, populate some mock data

--- src/features/cart/mock__data.ts
+++ src/features/cart/mock__data.ts
@@ -1,5 +1,50 @@
-import { Products, Carts, Offers } from './types';
+import { Products, Carts, Offers, ProductCode, OfferCode } from './types';
 
-export const products: Products = {};
-export const carts: Carts = {};
-export const offers: Offers = {};
+export const products: Products = {
+  [ProductCode.CLASSIC]: {
+    name: 'Classic Ad',
+    description: 'Offers the most basic level of advertisement',
+    price: 269.99,
+  },
+  [ProductCode.STANDOUT]: {
+    name: 'Standout Ad',
+    description: 'Allows advertisers to use a company logo and use a longer presentation text',
+    price: 322.99,
+  },
+  [ProductCode.PREMIUM]: {
+    name: 'Premium Ad',
+    description: 'Same benefits as StandOut, but also puts the advertisement at the top of the results',
+    price: 394.99,
+  },
+};
+
+export const offers: Offers = {
+  [OfferCode.SECONDBITE_3FOR2CLASSIC]: {
+    eligibleProduct: ProductCode.CLASSIC,
+    threshold: 3,
+    discountAmount: products[ProductCode.CLASSIC].price,
+  },
+  [OfferCode.AXIL_STANDOUT]: {
+    eligibleProduct: ProductCode.STANDOUT,
+    threshold: 1,
+    discountAmount: (products[ProductCode.STANDOUT].price - 299.99),
+  },
+  [OfferCode.MYER_5FOR4STANDOUT]: {
+    eligibleProduct: ProductCode.STANDOUT,
+    threshold: 5,
+    discountAmount: products[ProductCode.STANDOUT].price,
+  },
+  [OfferCode.MYER_PREMIUM]: {
+    eligibleProduct: ProductCode.PREMIUM,
+    threshold: 1,
+    discountAmount: (products[ProductCode.STANDOUT].price - 389.99),
+  },
+};
+
+export const carts: Carts = {
+  1: {
+    customer: "Guest",
+    items: [ProductCode.CLASSIC, ProductCode.STANDOUT, ProductCode.PREMIUM],
+    offers: [],
+  },
+};
--- src/features/cart/types.ts
+++ src/features/cart/types.ts
@@ -1,8 +1,34 @@
-type Product = {};
-export type Products = Record<string, Product>;
+export enum ProductCode {
+  CLASSIC = 'CLASSIC',
+  STANDOUT = 'STANDOUT',
+  PREMIUM = 'PREMIUM',
+}
 
-type Cart = {};
-export type Carts = Record<string, Cart>;
+interface Product {
+  name: string;
+  description: string;
+  price: number;
+}
 
-type Offer = {};
-export type Offers = Record<string, Offer>;
+export type Products = Record<ProductCode, Product>;
+
+export enum OfferCode {
+  SECONDBITE_3FOR2CLASSIC = 'SECONDBITE_3FOR2CLASSIC',
+  AXIL_STANDOUT = 'AXIL_STANDOUT',
+  MYER_5FOR4STANDOUT = 'MYER_5FOR4STANDOUT',
+  MYER_PREMIUM = 'MYER_PREMIUM',
+}
+
+interface Offer {
+  eligibleProduct: ProductCode;
+  threshold: 1 | 2 | 3 | 4 | 5;
+  discountAmount: number;
+}
+export type Offers = Record<OfferCode, Offer>;
+
+interface Cart {
+  customer: string;
+  items: ProductCode[];
+  offers: Offer[];
+};
+export type Carts = Record<number, Cart>;
